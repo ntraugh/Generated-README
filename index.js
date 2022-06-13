d55d6af966793d410f3844c45991f19d7144c344
Nate Traugh <n.traugh@yahoo.com>
2022-06-13 13:03:09 -0400

    added packages, created questions for the README

---
+++ index.js
@@ -0,0 +1,57 @@
+// TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const newMarkdown = require("./generateMarkdown")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input", 
+        name: "title",
+        message: "What is your projects title?", 
+    },
+    {
+        type: "input", 
+        name: "description",
+        message: "Describe your project.", 
+    },
+    {
+        type: "input", 
+        name: "installation",
+        message: "Explain how to install the software",  
+    },
+    {
+        type: "input", 
+        name: "usage",
+        message: "Describe how you will use the project.", 
+    },
+    {
+        type: "input", 
+        name: "license",
+        message: "Select a license for the project.", 
+    },
+    {
+        type: "input", 
+        name: "contributing",
+        message: "Who contributed to the project?", 
+    },
+    {
+        type: "input", 
+        name: "tests",
+        message: "Enter testing instructions for the project.", 
+    },
+    {
+        type: "input", 
+        name: "questions",
+        message: "Enter any FAQ.", 
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
