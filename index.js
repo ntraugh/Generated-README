2526451ac58b7d096be3f0a76e7d569afa11a564
Nate Traugh <n.traugh@yahoo.com>
2022-06-13 13:37:24 -0400

    installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-const fs = require("fs")
-const inquirer = require("inquirer")
-const newMarkdown = require("./generateMarkdown")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const newMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -55,3 +55,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
