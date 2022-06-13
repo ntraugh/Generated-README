8924f92dc49b8375ead8c33846e59a2cfdd64389
Nate Traugh <n.traugh@yahoo.com>
2022-06-13 19:02:58 -0400

    took the dash out of the generateMarkdown README sections

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -25,13 +25,13 @@ function generateMarkdown(data) {
   ${renderLicenseBadge(data.license)}
 
   ## Table of Contents
-  - [Description](#-Description)
-  - [Installation](#-Installation)
-  - [Usage](#-Usage)
-  - [License](#-License)
-  - [Contributing](#-Contributing)
-  - [Tests](#-Tests)
-  - [Questions](#-Questions)
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
 
   ## Description
   ${data.description}
